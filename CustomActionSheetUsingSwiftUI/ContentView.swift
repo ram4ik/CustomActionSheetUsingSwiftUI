fe2fb2dbe0e669c668dd0f98ff6d112b754284bc
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-01-09 23:31:16 +0200

    init

--- CustomActionSheetUsingSwiftUI/ContentView.swift
+++ CustomActionSheetUsingSwiftUI/ContentView.swift
@@ -9,13 +9,72 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var show = false
+    
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            Button(action: {
+                self.show.toggle()
+            }) {
+                Text("Action Sheet")
+            }
+            
+            VStack {
+                Spacer()
+                CustomActionSheet()
+                    .offset(y: self.show ? 0 : UIScreen.main.bounds.height)
+            }
+            .background((self.show ? Color.black.opacity(0.3) : Color.clear)
+            .edgesIgnoringSafeArea(.all)
+            .onTapGesture {
+                self.show.toggle()
+            })
+            .edgesIgnoringSafeArea(.bottom)
+        }.animation(.default)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct CustomActionSheet: View {
+    @State var show1 = false
+    @State var show2 = false
+    @State var show3 = false
+    @State var show4 = false
+    @State var count = 0
+    
+    var body: some View {
+        VStack(spacing: 15) {
+            Toggle(isOn: self.$show1) {
+                Text("Notifications")
+            }
+            Toggle(isOn: self.$show2) {
+                Text("Likes")
+            }
+            Toggle(isOn: self.$show3) {
+                Text("Comments")
+            }
+            Toggle(isOn: self.$show4) {
+                Text("Subscribers Count")
+            }
+            Stepper(onIncrement: {
+                self.count += 1
+            }, onDecrement: {
+                if self.count != 0 {
+                    self.count -= 1
+                }
+            }) {
+                Text("Notification Limit = \(self.count)")
+            }
+        }
+        .padding(.bottom, (UIApplication.shared.windows.last?.safeAreaInsets.bottom)! + 10)
+        .padding(.horizontal)
+        .padding(.top, 20)
+        .background(Color.white)
+        .cornerRadius(25)
     }
 }
